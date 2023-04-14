b07196e95955efa3a6e7f783328876a94ddadf32
Solovev Anton <solovev.anton93@gmail.com>
2023-04-14 15:06:11 +0300

    Galaxy class finished

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collection;
 
 /*
 *Class with main method to test other classes
@@ -6,23 +7,31 @@ import java.util.Arrays;
 public class Test {
     public static void main(String args[]){
     Planet[] planets = {
+            null,
         new Planet(),
         new Planet("corrupted", 1,1),
         new Planet("Earth", 6_378,23.933),
-        new Planet("Earth", 6_378,23.933)
+        new Planet("Earth", 100,10)
     };
 
     Galaxy[] galaxies = {
             new Galaxy(),
+            new Galaxy("nullPlanet", null,null),
             new Galaxy("empty"),
             new Galaxy("1 plan",new Planet()),
-            new Galaxy("2 plan",new Planet(),new Planet("Earth", 6_378,23.933)),
+            new Galaxy("2 plan",new Planet(),new Planet("otherPlan", 6_378,23.933)),
             new Galaxy("array",planets),
-            new Galaxy("collection", Arrays.asList(planets))
+            new Galaxy("collection", Arrays.asList(planets)),
     };
 
     for(Galaxy g : galaxies){
+        System.out.println(g.getName());
+        System.out.println(g.behavior());
+        System.out.println(g.addPlanet(null));
+        System.out.println(g.deletePlanet((String)null));
+        System.out.println(g.deletePlanet((Planet)null));
         System.out.println(g);
+        System.out.println();
     }
 
     }
