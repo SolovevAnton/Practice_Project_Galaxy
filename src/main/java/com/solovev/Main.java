20ca2061a2d61559ba10cac5d672c5856c72220c
Solovev Anton <solovev.anton93@gmail.com>
2023-05-08 18:02:15 +0300

    Fixed issue with XML saving Ready to archive

--- src/main/java/com/solovev/Main.java
+++ src/main/java/com/solovev/Main.java
@@ -38,64 +38,64 @@ public class Main {
     }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException {
-//        Universe u = new Universe("Random");
-//        int numGenerated = 3;
-//        int maxRadOrb = 100;
-//        int maxOrb = 10;
-//        char GalaxyName = 'G';
-//        char PlanetName = 'P';
-//        IntStream
-//                .range(0, numGenerated)
-//                .forEach(i -> u.addGalaxy(RandomGenerator.generateGalaxy(GalaxyName,
-//                        PlanetName, numGenerated, maxRadOrb, maxOrb)));
-//        Galaxy g = new Galaxy("Random Gal");
-//        IntStream
-//                .range(0, numGenerated)
-//                .forEach(i -> g.addPlanet(RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb)));
-//        Planet p = RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb);
-//        Planet toBeDeleted = RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb);
-//        Planet notExistent = new Planet("NotAPlanet", 1, 1);
-//
-//        System.out.println(p.behavior());
-//        System.out.println(g.behavior());
-//        //Tests
-//        test(true, g.addPlanet(p));
-//        test(false, g.addPlanet(p));
-//
-//        test(p, g.searchPlanet(p.getName()));
-//        test(null, g.searchPlanet(notExistent.getName()));
-//
-//        test(g.size() - 1, g.searchPlanet(p));
-//        test(-1, g.searchPlanet(notExistent));
-//
-//        g.addPlanet(toBeDeleted);
-//        test(toBeDeleted, g.deletePlanet(toBeDeleted.getName()));
-//        test(null, g.deletePlanet(toBeDeleted.getName()));
-//
-//        g.addPlanet(toBeDeleted);
-//        test(true, g.deletePlanet(toBeDeleted));
-//        test(false, g.deletePlanet(toBeDeleted));
-//
-//        test(true, u.addGalaxy(g));
-//        test(false, u.addGalaxy(g));
-//
-//        test(g, u.searchGalaxy(g.getName()));
-//        test(null, u.searchGalaxy("Earth"));
-//
-//        test(p, u.searchPlanet(p.getName()));
-//        test(null, u.searchPlanet(notExistent.getName()));
-//
-//        test(u.size() - 1, u.searchGalaxy(g));
-//        test(-1, u.searchGalaxy(new Galaxy()));
-//
-//        //tests for arrays both should return true
-//        System.out.println(
-//                Arrays.equals(new int[]{u.size() - 1, g.size() - 1}, u.searchPlanet(p)));
-//        System.out.println(
-//                Arrays.equals(new int[0], u.searchPlanet(notExistent)));
-//
+        Universe u = new Universe("Random");
+        int numGenerated = 3;
+        int maxRadOrb = 100;
+        int maxOrb = 10;
+        char GalaxyName = 'G';
+        char PlanetName = 'P';
+        IntStream
+                .range(0, numGenerated)
+                .forEach(i -> u.addGalaxy(RandomGenerator.generateGalaxy(GalaxyName,
+                        PlanetName, numGenerated, maxRadOrb, maxOrb)));
+        Galaxy g = new Galaxy("Random Gal");
+        IntStream
+                .range(0, numGenerated)
+                .forEach(i -> g.addPlanet(RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb)));
+        Planet p = RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb);
+        Planet toBeDeleted = RandomGenerator.generatePlanet(PlanetName, maxRadOrb, maxOrb);
+        Planet notExistent = new Planet("NotAPlanet", 1, 1);
+
+        System.out.println(p.behavior());
+        System.out.println(g.behavior());
+        //Tests
+        test(true, g.addPlanet(p));
+        test(false, g.addPlanet(p));
+
+        test(p, g.searchPlanet(p.getName()));
+        test(null, g.searchPlanet(notExistent.getName()));
+
+        test(g.size() - 1, g.searchPlanet(p));
+        test(-1, g.searchPlanet(notExistent));
+
+        g.addPlanet(toBeDeleted);
+        test(toBeDeleted, g.deletePlanet(toBeDeleted.getName()));
+        test(null, g.deletePlanet(toBeDeleted.getName()));
+
+        g.addPlanet(toBeDeleted);
+        test(true, g.deletePlanet(toBeDeleted));
+        test(false, g.deletePlanet(toBeDeleted));
+
+        test(true, u.addGalaxy(g));
+        test(false, u.addGalaxy(g));
+
+        test(g, u.searchGalaxy(g.getName()));
+        test(null, u.searchGalaxy("Earth"));
+
+        test(p, u.searchPlanet(p.getName()));
+        test(null, u.searchPlanet(notExistent.getName()));
+
+        test(u.size() - 1, u.searchGalaxy(g));
+        test(-1, u.searchGalaxy(new Galaxy()));
+
+        //tests for arrays both should return true
+        System.out.println(
+                Arrays.equals(new int[]{u.size() - 1, g.size() - 1}, u.searchPlanet(p)));
+        System.out.println(
+                Arrays.equals(new int[0], u.searchPlanet(notExistent)));
+
 //        u.behavior();
-        File dir = new File("D:\\Git\\Practice_Projects\\Archive\\Galaxy\\src\\main\\resources");
+        File dir = new File("src\\main\\resources");
 
         File universeFileIn = new File(dir,"UniverseInput.xml");
         File fOut = new File(dir,"UniverseMyOut.xml");
--- src/main/java/com/solovev/util/Util.java
+++ src/main/java/com/solovev/util/Util.java
@@ -1,9 +1,7 @@
 package com.solovev.util;
 
-import com.solovev.model.Planet;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -11,14 +9,15 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
     public static void transform(File fileIn, File fileOut) throws ParserConfigurationException, IOException, SAXException, TransformerException {
@@ -36,6 +35,7 @@ public class Util {
 
         Document documentOut = documentBuilder.newDocument();
         Element planetsOut = documentOut.createElement("Planets");
+        documentOut.appendChild(planetsOut);
         int i = 1;
         for (Map.Entry<String, ArrayList<Element>> e : planets.entrySet()) {
             Element equalGroup = documentOut.createElement("EqualGroup");
